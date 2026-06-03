ac0347088900212c7a7d1c082052369fddc2a2b8
Juyoung Shin <126848983+jyshin9@users.noreply.github.com>
2026-06-03 16:23:42 +0900

    BookManager 수정

--- JenkinsProject/src/BookManager.java
+++ JenkinsProject/src/BookManager.java
@@ -1,5 +1,14 @@
 public class BookManager {
+    
+    // 도서 관리 클래스
+    private String bookTitle;
+    
     public void addBook(String title) {
+        this.bookTitle = title;
         System.out.println("Book added: " + title);
+    }
+    
+    public String getBook() {
+        return bookTitle;
     }
 }
